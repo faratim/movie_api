66ccea0756f7d28302a97d664d48c4c9f293d499
faratim <104940465+faratim@users.noreply.github.com>
2022-08-08 15:14:00 -0400

    Added skeletons of endpoints

--- index.js
+++ index.js
@@ -55,9 +55,48 @@ app.get('/', (req, res) => {
     res.send('Welcome to myFlix!')
 });
 
+// GET list of all movies
 app.get('/movies', (req, res) => {
-    res.json(topMovies);
+    res.json(allMovies);
 });
+
+// GET data about a single movie
+app.get('/movies/:title', (req, res) => {
+    res.send('GET request returning all data for a specific movie.');
+});
+
+// GET data about a genre by name
+app.get('/movies/genres/:name', (req, res) => {
+    res.send('GET request returning all data for a specific genre.');
+});
+
+// GET data about a Director by name
+app.get('/movies/directors/:name', (req, res) => {
+    res.send('GET request returning all data for a specific director.');
+});
+
+// POST a new user to the user list
+app.post('/users', (req, res) => {
+    res.send(`POST request that checks for existing user, if user doesn't exist, it adds the user to the list`);
+})
+
+// POST a new movie to the favorites list
+app.post('/users/:name/movies/:MovieID', (req, res) => {
+    res.send('POST new movie name to list of favorite movies');
+});
+
+// DELETE a movie from the favorites list
+app.delete('/users/:name/movies/:MovieID', (req, res) => {
+    res.send('DELETE a movie from the favorites list.');
+});
+
+// DELETE a user from the database
+app.delete('/users/:name', (req, res) => {
+    res.send('DELETE a user from the database.');
+});
+
+
+
 
 //Error Handler
 app.use((err, req, res, next) => {
