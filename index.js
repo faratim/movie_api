0536d4d849c8d54abbcad78833d233b125773417
faratim <104940465+faratim@users.noreply.github.com>
2022-09-27 23:19:15 -0400

    debugging

--- index.js
+++ index.js
@@ -18,9 +18,10 @@ const app = express();
 const Movies = Models.Movie;
 const Users = Models.User;
 // const URI = 'mongodb://localhost:27017/movie-api'; // Database Option 1: Local DB
-// const URI = process.env.CONNECTION_URI; // Database Option 2: Hosted DB
+const URI = process.env.CONNECTION_URI; // Database Option 2: Hosted DB
+// "mongodb+srv://faratim:timfara@myflixdb.iymuhs6.mongodb.net/?retryWrites=true&w=majority"
 
-mongoose.connect("mongodb+srv://faratim:timfara@myflixdb.iymuhs6.mongodb.net/?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // File logging
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
